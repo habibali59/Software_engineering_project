a14c3f476b43595b1a1f9d82cdf6861bf2db9eed
Ammar <160019476+Ammarabbas2002@users.noreply.github.com>
2024-05-03 19:33:05 +0300

    Update and rename footer.html to footer.php

---
+++ html/footer.php
@@ -0,0 +1,33 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>روابط سريعه</h3>
+         <a href="#"> <i class="fas fa-angle-right"></i> Management system</a>
+        
+      </div>
+
+      <div class="box">
+         <h3>روابط اضافيه</h3>
+         <a href="#"> <i class="fas fa-angle-right"></i> weclocum</a>
+        
+      </div>
+
+      <div class="box">
+         <h3>اتصل بنا</h3>
+         
+         <a href="#"><i class="fas fa-map-marker-alt"></i> Ammar, Iraq - 400104 </a>
+      </div>
+
+      <div class="box">
+         <h3>تابعنا</h3>
+         <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
+        
+      </div>
+
+   </section>
+
+   
+
+</footer>
